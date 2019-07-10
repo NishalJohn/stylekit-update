5bbf22a337829584f9e6179aa130637cf1fb380f
Azury <nishal.john24@gmail.com>
2019-07-10 21:15:43 +0530

    Fix from Aiyya

--- stylekit-updater.js
+++ stylekit-updater.js
@@ -25,9 +25,12 @@ function cacheMediaQueries(oldJson) {
     this.mediaQ_480 = oldJson['@media (max-width : 480px)'];
     this.mediaQ_360 = oldJson['@media (max-width : 360px)'];
 }
-
+var core_stuff = [".navbar",".navbar-nav",".navbar-text","html, body",".section",".multiLayered",".multiLayered.bgVideo",".container",".container-fluid",".row",".column",".container, .container-fluid",".inline-row",".box"]
 function mergeStylekit(newJSON) {
-    newJSON['@media screen, print'] = this.mediaQ_screenPrint;
+    for(var i = 0;i<core_stuff.length;i++){
+        this.mediaQ_screenPrint[core_stuff[i]] = Object.assign({},newJSON['@media screen, print'][core_stuff[i]]);
+    }
+    newJSON['@media screen, print'] = $.extend(true,newJSON['@media screen, print'],this.mediaQ_screenPrint);
     newJSON['@media (max-width : 1600px)'] = this.mediaQ_1600;
     newJSON['@media (max-width : 1200px)'] = this.mediaQ_1200;
     newJSON['@media (max-width : 993px)'] = this.mediaQ_993;
@@ -36,9 +39,9 @@ function mergeStylekit(newJSON) {
     newJSON['@media (max-width : 360px)'] = this.mediaQ_360;
     var jsonString = JSON.stringify(newJSON)
     var ugly = jsonString;
-    var obj = JSON.parse(ugly);
-    var pretty = JSON.stringify(obj, undefined, 4);
-    $('#compiledJSON').text(pretty)
+    //var obj = JSON.parse(ugly);
+    //var pretty = JSON.stringify(obj, undefined, 4);
+    $('#compiledJSON').text(ugly)
     $('#download').removeClass('hide');
     this.jsonString = jsonString;
     console.log(jsonString);
